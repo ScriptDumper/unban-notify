3bd5a02670090b3f2f15c41cbde49c4997f5a7d0
ScriptDumper <118000363+ScriptDumper@users.noreply.github.com>
2024-08-14 13:47:09 +0900

    Update flask.py

--- flask.py
+++ flask.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask,render_template
+from threading import Thread
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return "Bot is running!"
+def index():
+    return "It's Running!"
 
-if __name__ == "__main__":
+def run():
     app.run(host='0.0.0.0', port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
